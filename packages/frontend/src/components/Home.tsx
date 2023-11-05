289b7941c4778815407153b4c78b23acb32c8fea
Ariel <arielmalada@outlook.com>
2023-11-06 00:30:19 +0200

    feat: first integration

--- packages/frontend/src/components/Home.tsx
+++ packages/frontend/src/components/Home.tsx
@@ -1,5 +1,20 @@
+import dynamic from "next/dynamic";
+
+const ResponsivePie = dynamic(
+  () => import("@nivo/pie").then((m) => m.ResponsivePie),
+  { ssr: false },
+);
+
+import Data1 from "../../public/data1.json";
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <div style={{ height: "400px" }}>
+        <ResponsivePie data={Data1} />
+      </div>
+    </>
+  );
 };
 
 export default Home;
