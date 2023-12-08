21867baae49a6009e02dbc5dd6dccef23be4d7c3
arielmalada <arielmalada@outlook.com>
2023-12-09 00:07:11 +0200

    feat: home final

--- packages/frontend/src/components/Home.tsx
+++ packages/frontend/src/components/Home.tsx
@@ -11,208 +11,113 @@ import LineAverage from "./Charts/LineAverage";
 import { Box, Grid, Card, CardContent, Typography } from "@mui/material";
 import { MeasurementContext } from "@/contexts/MeasurementProvider.context";
 import { useContext, useEffect, useState } from "react";
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import Dot from "./Dot";
-import LineCompliance from "./Charts/LineCompliance";
-import LineInterrupted from "./Charts/LineInterrupted";
+import { subDays } from 'date-fns'
+
 const Home = () => {
-  const { maxDate, minDate } = useContext(MeasurementContext);
-  // state for the date range of line average chart
-  const [minimumDate, setMinimumDate] = useState<Date | undefined | null>(minDate);
-  const [maximumDate, setMaximumDate] = useState<Date | undefined | null>(maxDate);
-
-  useEffect(() => {
-    setMinimumDate(minDate);
-    setMaximumDate(maxDate);
-  }, [minDate, maxDate]);
-
+  const { maxDate, statisticsNumbers } = useContext(MeasurementContext);
+  const minimumDate = maxDate && subDays(maxDate, 60);
+  const percentage = statisticsNumbers && Math.round(statisticsNumbers.compliance / statisticsNumbers.total * 100);
   return (
     <>
       <h1>Hi, Welcome</h1>
-      <div className="flex justify-end mb-4">
-        <span className="m-3">
-          <DatePicker
-            minDate={minDate}
-            maxDate={maximumDate}
-            defaultValue={minDate}
-            value={minimumDate}
-            onChange={(minimumDate) => setMinimumDate(minimumDate)}
-          />
-        </span>
-        <span className="m-3">
-          <DatePicker
-            minDate={minimumDate}
-            maxDate={maxDate}
-            defaultValue={maxDate}
-            value={maximumDate}
-            onChange={(maximumDate) => setMaximumDate(maximumDate)}
-          />
-        </span>
-      </div>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={12} md={6} lg={6}>
-          <Card
-            sx={{
-              padding: (theme) => theme.spacing(2),
-            }}
-          >
-            <CardContent
-              sx={{
-                height: (theme) => theme.spacing(75),
-              }}
-            >
-              <Typography
-                variant="h4"
-                sx={{
-                  marginBottom: (theme) => theme.spacing(2),
-                }}
-                component="div"
-              >
-                Complete & Interupted Role
-              </Typography>
-              <div className="flex justify-between pt-4">
-                <span>COMPLETE</span>
-                <span>INTERRUPTED</span>
-              </div>
-              <Box
-                sx={{ height: (theme) => theme.spacing(50) }}
-                paddingX={1}
-                paddingY={1}
-              >
-                <div className="h-full">
-                  <StackedCompliance />
-                </div>
-              </Box>
-              <div className="flex flex-wrap pt-4">
-                <div>
-                  <Dot color="rgb(241, 225, 91)" />
-                  <span className="p-2">Doctors</span>
-                </div>
-                <div>
-                  <Dot color="rgb(244, 117, 96)" />
-                  <span className="p-2">Nurses</span>
-                </div>
-                <div>
-                  <Dot color="rgb(232, 193, 160)" />
-                  <span className="p-2">Student Doctors</span>
-                </div>
-              </div>
+        <Grid item xs={2} md={2} >
+          <Card sx={{ height: '400px' }}>
+            <CardContent>
+              <Grid container spacing={1}>
+                <Grid item sx={{ textAlign: 'center' }} xs={12}>
+                  <Typography variant="h1">{percentage} %</Typography>
+                  <Typography>Compliance Percentage</Typography>
+                </Grid>
+                <Grid item sx={{ textAlign: 'center' }} xs={12}>
+                  <Typography variant="h1">{statisticsNumbers?.compliance}</Typography>
+                  <Typography>Compliance Count</Typography>
+                </Grid>
+                <Grid item sx={{ textAlign: 'center' }} xs={12}>
+                  <Typography variant="h1">{Math.round(statisticsNumbers?.averageTimeSpent || 0)}</Typography>
+                  <Typography>Average Time Spent</Typography>
+                </Grid>
+                <Grid item sx={{ textAlign: 'center' }} xs={12}>
+                  <Typography variant="h1">{statisticsNumbers?.total}</Typography>
+                  <Typography>Total Measurement</Typography>
+                </Grid>
+              </Grid>
             </CardContent>
           </Card>
         </Grid>
+        <Grid container item xs={10} spacing={3}>
+          <Grid item xs={12} md={6}>
+            <Card sx={{ height: '400px' }}>
+              <CardContent>
+                <Typography
+                  variant="h4"
+                  sx={{
+                    marginBottom: 2,
+                  }}
+                  component="div"
+                >
+                  Count of Status
+                </Typography>
+                <Box sx={{ height: '300px' }}>
+                  <ResponsivePie data={Data1} />
+                </Box>
 
-        <Grid item xs={12} sm={12} md={6} lg={6}>
-          <Card
-            sx={{
-              padding: (theme) => theme.spacing(2),
-            }}
-          >
-            <CardContent
-              sx={{
-                height: (theme) => theme.spacing(75),
-                paddingBottom: (theme) => theme.spacing(3),
-              }}
-            >
-              <Typography
-                variant="h4"
-                sx={{
-                  marginBottom: (theme) => theme.spacing(2),
-                }}
-              >
-                Average Time Spent / Day for Each Role
-              </Typography>
-              <Box
-                sx={{ height: (theme) => theme.spacing(45) }}
-                paddingX={1}
-                paddingY={1}
-              >
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <Card sx={{ height: '400px' }}>
+              <CardContent>
+                <Typography
+                  variant="h4"
+                  sx={{
+                    marginBottom: (theme) => theme.spacing(2),
+                  }}
+                  component="div"
+                >
+                  Complete & Interupted Role
+                </Typography>
+
+                <Box
+                  sx={{ height: '300px' }}>
+                  <div className="h-full">
+                    <StackedCompliance />
+                  </div>
+                </Box>
+
+              </CardContent>
+            </Card>
+          </Grid>
+
+        </Grid>
+        <Grid item xs={12} md={12} >
+          <Card sx={{ height: '600px' }}>
+            <CardContent>
+              <Box>
+                <Typography variant="h4">
+                  Average Time Spent / Day for Each Role
+                </Typography>
+                <Typography>
+                  Each day is counted by the average time spent of each role
+                </Typography>
+              </Box>
+
+              <Box sx={{ height: '450px' }}>
                 <div className="w-full h-full">
                   <LineAverage
                     minimumDate={minimumDate}
-                    maximumDate={maximumDate}
+                    maximumDate={maxDate}
                   />
+
                 </div>
               </Box>
             </CardContent>
           </Card>
         </Grid>
 
-        <Grid item xs={12} sm={12} md={6} lg={6}>
-          <Card
-            sx={{
-              padding: (theme) => theme.spacing(2),
-            }}
-          >
-            <CardContent
-              sx={{
-                height: (theme) => theme.spacing(75),
-                paddingBottom: (theme) => theme.spacing(3),
-              }}
-            >
-              <Typography
-                variant="h4"
-                sx={{
-                  marginBottom: (theme) => theme.spacing(2),
-                }}
-                component="div"
-              >
-                Count of Status
-              </Typography>
-              <Box
-                sx={{ height: (theme) => theme.spacing(55) }}
-                paddingX={1}
-                paddingY={1}
-              >
-                <ResponsivePie data={Data1} />
-              </Box>
-              <div className="flex flex-wrap pt-4">
-                <div>
-                  <Dot color="rgb(244, 117, 96)" />
-                  <span className="p-2">Interupted</span>
-                </div>
-                <div>
-                  <Dot color="rgb(232, 193, 160)" />
-                  <span className="p-2">Completed</span>
-                </div>
-              </div>
-            </CardContent>
-          </Card>
-        </Grid>
 
-        <Grid item xs={12} sm={12} md={6} lg={6}>
-          <Card
-            sx={{
-              padding: (theme) => theme.spacing(2),
-            }}
-          >
-            <CardContent
-              sx={{
-                height: (theme) => theme.spacing(75),
-                paddingBottom: (theme) => theme.spacing(3),
-              }}
-            >
-              <Typography
-                variant="h4"
-                sx={{
-                  marginBottom: (theme) => theme.spacing(2),
-                }}
-                component="div"
-              >
-                All each role avg for each day
-              </Typography>
-              <Box
-                sx={{ height: (theme) => theme.spacing(50) }}
-                paddingX={1}
-                paddingY={1}
-              >
-                <LineCompliance
-                  minimumDate={minimumDate}
-                  maximumDate={maximumDate}
-                />
-              </Box>
-            </CardContent>
-          </Card>
-        </Grid>
+
       </Grid>
     </>
   );
