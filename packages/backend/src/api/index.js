40129e639d47b363611a74b0709e347934b1acd2
yinkagold <yinkagolden@gmail.com>
2023-12-09 16:02:59 +0200

    chart service

--- packages/backend/src/api/index.js
+++ packages/backend/src/api/index.js
@@ -3,10 +3,12 @@ const { isAuthenticated } = require('../middlewares');
 
 const auth = require('./auth/auth.routes');
 const users = require('./users/users.routes');
+const data = require('./data/data.routes');
 
 const router = express.Router();
 
 router.use('/auth', auth);
 router.use('/users', isAuthenticated, users);
+router.use('/data', data);
 
 module.exports = router;
