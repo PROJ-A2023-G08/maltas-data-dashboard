ca577e48ddfe5daef4f1c4bbadfcd661b9a997a0
arielmalada <arielmalada@outlook.com>
2023-12-14 16:07:34 +0200

    fix: ts config

--- packages/frontend/jest.config.js
+++ packages/frontend/jest.config.js
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  globals: {
+    "ts-jest": {
+      "tsconfig": "./tsconfig.test.json"
+    }
+  },
   preset: "ts-jest",
   testEnvironment: "jsdom",
   moduleDirectories: ["node_modules", "src"],
